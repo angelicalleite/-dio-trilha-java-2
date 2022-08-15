9ff27df1413f59373d828caf3f0385c5ae8651d0
angelica.santos <angelica.santos@cnj.jus.br>
2022-08-15 15:11:43 -0300

    Projeto

--- src/Contador.java
+++ src/Contador.java
@@ -24,7 +24,7 @@ public class Contador {
 
         int contagem = parametroDois - parametroUm;
 
-        for (int i = 0; i <= contagem; ++i) {
+        for (int i = 1; i <= contagem; i++) {
             System.out.println("Imprimindo o número " + i);
         }
     }
